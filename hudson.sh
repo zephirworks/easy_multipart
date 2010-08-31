d34617111af0f9d868d5192450c21247e8698723
Andrea Campi <andrea.campi@zephirworks.com>
2010-08-31 09:29:36 +0200

    Improve the hudson script a bit.

--- hudson.sh
+++ hudson.sh
@@ -5,13 +5,16 @@ set -e
 [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 
 if [[ -z ${RUBY_RUNTIME} ]]; then
+  RUBY_RUNTIME=$1
+fi
+if [[ -z ${RUBY_RUNTIME} ]]; then
   RUBY_RUNTIME=1.8.7
 fi
 
 if [[ -n "${JOB_NAME}" ]] ; then
   GEMSET=`echo ${JOB_NAME} | sed "s!/RUBY_RUNTIME=.*!!" | sed "s/ /_/g"`
 else
-  GEMSET=easy_multipart
+  GEMSET=`basename $PWD`
 fi
 
 if [[ -z "$(rvm list | grep $RUBY_RUNTIME)" ]] ; then
@@ -21,6 +24,8 @@ fi
 rvm use ${RUBY_RUNTIME} && \
   rvm --force gemset delete $GEMSET && \
   rvm gemset create $GEMSET && \
-  rvm gemset use $GEMSET && \
-  gem install rails --version "2.3.8" --no-ri --no-rdoc && \
-  rake -t test
+  rvm gemset use $GEMSET || 0
+
+gem install rails --version "2.3.8" --no-ri --no-rdoc
+
+rake -t test
